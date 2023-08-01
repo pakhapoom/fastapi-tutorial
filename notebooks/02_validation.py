92de33d9591017982a17bacd392dc2aa8096ecc8
Pakhapoom <pakhapoom.sar@gmail.com>
2023-08-01 10:16:26 +0700

    feat: add status code for successeful requests

--- notebooks/02_validation.py
+++ notebooks/02_validation.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
-from starlette import status
+from starlette import status # return in response headers
 
 app = FastAPI()
 
@@ -56,11 +56,11 @@ BOOKS = [
 ]
 
 
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK) # once the request is successfully processed, it returns 200 OK status.
 async def read_all_books():
     return BOOKS
 
-@app.get("/books/publish/")
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
 async def read_books_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
     books_to_return = []
     for book in BOOKS:
@@ -68,7 +68,7 @@ async def read_books_by_publish_date(published_date: int = Query(gt=1999, lt=203
             books_to_return.append(book)
     return books_to_return
 
-@app.post("/create-book")
+@app.post("/create-book", status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump()) # .dict() in pydantic1
     BOOKS.append(find_book_id(new_book))
@@ -77,13 +77,14 @@ def find_book_id(book: Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
 
-@app.get("/books/{book_id}")
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
 async def read_book(book_id: int = Path(gt=0)): # validate the path parameter to accept only those > 0
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=404, detail="Item not found")
 
-@app.get("/books/")
+@app.get("/books/", status_code=status.HTTP_200_OK)
 async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)): # validate the query parameter
     books_to_return = []
     for book in BOOKS:
@@ -91,15 +92,23 @@ async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)): # validate 
             books_to_return.append(book)
     return books_to_return
 
-@app.put("/books/update_book")
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book: BookRequest):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
 
-@app.delete("/books/{book_id}")
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
+            book_changed = True
             break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
