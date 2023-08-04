6478c717e5eb8ab3a99fb0b120c1d4a990b04e3f
Pakhapoom <pakhapoom.sar@gmail.com>
2023-08-05 05:27:17 +0700

    feat: update data models

--- fastapi_tutorial/models.py
+++ fastapi_tutorial/models.py
@@ -1,6 +1,17 @@
 from fastapi_tutorial.database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
 class Todos(Base):
     __tablename__ = "todos"
 
@@ -16,7 +27,7 @@ class Todos(Base):
         Boolean, 
         default=False,
     )
-    # owner_id = Column(
-    #     Integer,
-    #     ForeignKey("users.id")
-    # )
+    owner_id = Column(
+        Integer,
+        ForeignKey("users.id"),
+    )
